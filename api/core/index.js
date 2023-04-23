2759f15df09993797679f7304efd4fa11e77c23e
JonasSCruz <jonas.cruz@dbccompany.com.br>
2023-04-23 19:04:25 -0300

    test rota

--- api/core/index.js
+++ api/core/index.js
@@ -11,11 +11,7 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    app.use(cors())
-    next()
-})
+app.use(cors())
 
 require("./config/db.js");
 
