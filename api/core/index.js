017824579c9892b18ced0b10bb20c64c4428ac00
JonasSCruz <jonas.cruz@dbccompany.com.br>
2023-04-23 17:25:30 -0300

    test cors

--- api/core/index.js
+++ api/core/index.js
@@ -11,6 +11,10 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 
+app.use((req, res, next) => {
+    console.log("Acessou Middleware!")
+})
+
 app.use(cors());
 
 require("config/db.js");
