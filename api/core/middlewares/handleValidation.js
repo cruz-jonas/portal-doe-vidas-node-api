99756b5951d2bfeed786f0c425cae2d6ba75add9
JonasSCruz <jonas.cruz@dbccompany.com.br>
2023-04-23 21:20:24 -0300

    remove bohr workflow

--- api/core/middlewares/handleValidation.js
+++ api/core/middlewares/handleValidation.js
@@ -2,7 +2,6 @@ const { validationResult } = require("express-validator")
 
 const validate = (req, res, next) => {
 
-    console.log(req)
     const errors = validationResult(req)
 
     if(errors.isEmpty()) {
